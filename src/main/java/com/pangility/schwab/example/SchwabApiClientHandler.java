fd1b3ebd8eb610c8e0e07f0fb45ccf853d1033ab
Greg Porter <greg.porter70@gmail.com>
2024-08-23 12:38:17 -0500

    added initialization to handler example

--- src/main/java/com/pangility/schwab/example/SchwabApiClientHandler.java
+++ src/main/java/com/pangility/schwab/example/SchwabApiClientHandler.java
@@ -7,7 +7,6 @@ import com.pangility.schwab.api.client.marketdata.model.movers.Screener;
 import com.pangility.schwab.api.client.marketdata.model.quotes.QuoteResponse;
 import com.pangility.schwab.api.client.oauth2.SchwabAccount;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,26 +19,30 @@ public class SchwabApiClientHandler {
     @Autowired
     private SchwabMarketDataApiClient schwabMarketDataApiClient;
 
-    @Value("${schwab-api.oauth2.userId}")
-    private String schwabUserId;
+    private final String schwabUserId = "<YOUR-SCHWAB-ACCOUNT-USER-ID-GOES-HERE>";
 
     public void init() {
-        ClientTokenHandler clientTokenHandler = new ClientTokenHandler();
-        SchwabAccount schwabAccount = new SchwabAccount();
-        schwabAccount.setUserId(schwabUserId);
-        // If you have saved your refresh token, pass it to the API client service.
-        // schwabAccount.setRefreshToken(schwabRefreshToken);
-        // schwabAccount.setRefreshExpiration(schwabRefreshExpiration);
-        schwabMarketDataApiClient.init(schwabAccount, clientTokenHandler);
+        if(!schwabMarketDataApiClient.isInitialized()) {
+            ClientTokenHandler clientTokenHandler = new ClientTokenHandler();
+            SchwabAccount schwabAccount = new SchwabAccount();
+            schwabAccount.setUserId(schwabUserId);
+            // If you have saved your refresh token, pass it to the API client service.
+            // Otherwise, you will have to generate one each time you run this example.`
+            // schwabAccount.setRefreshToken(schwabRefreshToken);
+            // schwabAccount.setRefreshExpiration(schwabRefreshExpiration);
+            schwabMarketDataApiClient.init(schwabAccount, clientTokenHandler);
+        }
     }
 
     @GetMapping("/quote")
     public Mono<QuoteResponse> callQuote(@RequestParam String symbol) {
+        this.init();
         return schwabMarketDataApiClient.fetchQuoteToMono(symbol);
     }
 
     @GetMapping("/movers/nyse")
     public Flux<Screener> callMovers() {
+        this.init();
         MoversRequest moversRequest = MoversRequest.builder()
                 .withIndexSymbol(MoversRequest.IndexSymbol.NYSE)
                 .build();
